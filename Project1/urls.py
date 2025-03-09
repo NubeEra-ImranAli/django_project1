2815fd89b38b4641d004e3933a405028bf5b79d6
NubeEra-ImranAli <nubeera.imranali@gmail.com>
2025-03-09 09:10:48 +0530

    new changes

--- Project1/urls.py
+++ Project1/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path
-from app1.views import home
+from app1.views import *
 
 urlpatterns = [
     path('', home, name='home'),
+    path('index2', index2, name='index2'),
 ]
--- app1/views.py
+++ app1/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'app1/index.html')
+def index2(request):
+    return render(request, 'app1/index2.html')
